953a5a87e3ca259aedd7b767663b63e848b4a21c
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2023-06-08 18:17:12 +0300

    feat: add database request to getProductsById handler

--- product-service/src/db/products.ts
+++ product-service/src/db/products.ts
@@ -4,3 +4,10 @@ export const getProductsList = () =>
   pgClient('products')
     .join('stocks', 'products.id', 'stocks.product_id')
     .select('products.*', 'stocks.count');
+
+export const getProductsById = (productId: string) =>
+  pgClient('products')
+    .join('stocks', 'products.id', 'stocks.product_id')
+    .select('products.*', 'stocks.count')
+    .where('products.id', productId)
+    .first();
